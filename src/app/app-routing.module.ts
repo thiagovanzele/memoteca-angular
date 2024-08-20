0b773e0720c2cab6b161b2cc7e837e9bace7bc43
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-20 11:08:18 -0300

    feat: excluir pensamento

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
 
 const routes: Routes = [
   {
@@ -18,6 +19,11 @@ const routes: Routes = [
   {
     path: 'listarpensamento',
     component: ListarPensamentosComponent
+  },
+
+  {
+  path: 'pensamentos/excluirpensamento/:id',
+  component: ExcluirPensamentoComponent
   }
 ];
 
